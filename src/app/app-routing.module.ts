75011d6a28486de35e9822049af14e21e22d3329
iobodianskyi <ivan.obodiansky.ua@gmail.com>
2018-10-27 20:55:44 +0300

    Added hash for URL. Added main, header, sidebar and tasks skeleton components.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,14 +1,21 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './home/home.component';
+import { TasksComponent } from './main/tasks/tasks.component';
+import { MainComponent } from './main/main.component';
 
 const routes: Routes = [
   { path: '', component: HomeComponent, pathMatch: 'full' },
+  {
+    path: '', component: MainComponent, children: [
+      { path: 'tasks', component: TasksComponent },
+    ]
+  },
   { path: '**', redirectTo: '' }
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes, { useHash: true })],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,19 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
+import { TasksComponent } from './main/tasks/tasks.component';
+import { MainComponent } from './main/main.component';
+import { HeaderComponent } from './main/header/header.component';
+import { SidebarComponent } from './main/sidebar/sidebar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
+    HomeComponent,
+    TasksComponent,
+    MainComponent,
+    HeaderComponent,
+    SidebarComponent
   ],
   imports: [
     BrowserModule,
