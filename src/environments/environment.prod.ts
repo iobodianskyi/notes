98f93fe0b171bb92756d6b33b1c3cd166bf24016
iobodianskyi <ivan.obodiansky.ua@gmail.com>
2018-11-14 10:44:30 +0200

    Updated custom auth domain for production

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,7 +2,7 @@ export const environment = {
   production: true,
   firebase: {
     apiKey: "AIzaSyAyIHfs6g5BmafkattKi-Z1zUxYGeop34k",
-    authDomain: "notes-14a4a.firebaseapp.com",
+    authDomain: "notes.obodianskyi.com",
     databaseURL: "https://notes-14a4a.firebaseio.com",
     projectId: "notes-14a4a",
     storageBucket: "",
