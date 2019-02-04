4b51568ba430986185da218909615bd26a4ed764
iobodianskyi <ivan.obodiansky.ua@gmail.com>
2019-02-04 18:18:42 +0200

    Updated ReadMe, removed env configs

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,12 +5,12 @@
 export const environment = {
   production: false,
   firebase: {
-    apiKey: 'AIzaSyAyIHfs6g5BmafkattKi-Z1zUxYGeop34k',
-    authDomain: 'notes-14a4a.firebaseapp.com',
-    databaseURL: 'https://notes-14a4a.firebaseio.com',
-    projectId: 'notes-14a4a',
+    apiKey: '',
+    authDomain: '',
+    databaseURL: '',
+    projectId: '',
     storageBucket: '',
-    messagingSenderId: '943999868994'
+    messagingSenderId: ''
   }
 };
 
